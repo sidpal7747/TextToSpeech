f5617d13cb10e4820c632748457d9c6a083e60a5
SIDDHESH <sidpal7747@india.com>
2017-01-29 00:18:13 +0530

    New Updated.

--- app/src/main/java/com/example/siddhesh/texttospeech/History.java
+++ app/src/main/java/com/example/siddhesh/texttospeech/History.java
@@ -5,22 +5,29 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 /**
  * Created by SIDDHESH on 16-01-2017.
  */
 
-public class History extends AppCompatActivity {
+public class History extends AppCompatActivity implements TextToSpeech.OnInitListener {
     ListView listView;
     ArrayAdapter<String> arrayAdapter;
+    private TextToSpeech tts;
+    int result = 0;
     String[] values;
     SQLiteDatabase sqLiteDatabase;
     DBhelper dBhelper;
@@ -35,6 +42,7 @@ public class History extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("MyData",MODE_PRIVATE);
         editor = sharedPreferences.edit();
         dBhelper = new DBhelper(this);
+        tts = new TextToSpeech(this, this);
         his = (ImageView)findViewById(R.id.history_icon);
         menus = (ImageView)findViewById(R.id.menuic);
         listView = (ListView)findViewById(R.id.history);
@@ -61,6 +69,13 @@ public class History extends AppCompatActivity {
                     }
                 });
                 popupMenu.show();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = (String) parent.getItemAtPosition(position);
+                speakOut(str);
             }
         });
         his.setOnClickListener(new View.OnClickListener() {
@@ -95,4 +110,50 @@ public class History extends AppCompatActivity {
         startActivity(new Intent(History.this,MainActivity.class));
         finish();
     }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            //if TTS initialized than set language
+            result = tts.setLanguage(Locale.US);
+
+            // tts.setPitch(5); // you can set pitch level
+            // tts.setSpeechRate(2); //you can set speech speed rate
+
+            //check language is supported or not
+            //check language data is available or not
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Missing data", Toast.LENGTH_LONG).show();
+                //disable button
+               // btnSpeak.setEnabled(false);
+                listView.setClickable(false);
+            } else {
+                //if all is good than enable button convert text to speech
+                //btnSpeak.setEnabled(true);
+                listView.setClickable(true);
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+    private void speakOut(String text) {
+        if(result!=tts.setLanguage(Locale.US))
+        {
+            Toast.makeText(getApplicationContext(), "Enter right Words...... ", Toast.LENGTH_LONG).show();
+        }else
+        {
+            //speak given text
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
 }
